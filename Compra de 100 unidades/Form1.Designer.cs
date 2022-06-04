aa2119712a8ef25e023f218d9d62b138efc89b26
Ggabriel22 <bolanosgabriel2217@gmail.com>
2022-06-04 14:55:10 -0600

    Primer commit

---
+++ Compra de 100 unidades/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Compra_de_100_unidades
+{
+    partial class Compras
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo1 = new System.Windows.Forms.Label();
+            this.lblcalcular = new System.Windows.Forms.Label();
+            this.lblcantidad = new System.Windows.Forms.Label();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.lblprecio = new System.Windows.Forms.Label();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTitulo1
+            // 
+            this.lblTitulo1.AutoSize = true;
+            this.lblTitulo1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lblTitulo1.Location = new System.Drawing.Point(30, 20);
+            this.lblTitulo1.Name = "lblTitulo1";
+            this.lblTitulo1.Size = new System.Drawing.Size(0, 16);
+            this.lblTitulo1.TabIndex = 0;
+            // 
+            // lblcalcular
+            // 
+            this.lblcalcular.AutoSize = true;
+            this.lblcalcular.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcalcular.Location = new System.Drawing.Point(101, 61);
+            this.lblcalcular.Name = "lblcalcular";
+            this.lblcalcular.Size = new System.Drawing.Size(130, 16);
+            this.lblcalcular.TabIndex = 1;
+            this.lblcalcular.Text = "Calcular descuento";
+            // 
+            // lblcantidad
+            // 
+            this.lblcantidad.AutoSize = true;
+            this.lblcantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcantidad.Location = new System.Drawing.Point(49, 129);
+            this.lblcantidad.Name = "lblcantidad";
+            this.lblcantidad.Size = new System.Drawing.Size(74, 16);
+            this.lblcantidad.TabIndex = 2;
+            this.lblcantidad.Text = "Cantidad:";
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Location = new System.Drawing.Point(125, 125);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(74, 20);
+            this.txtcantidad.TabIndex = 3;
+            // 
+            // lblprecio
+            // 
+            this.lblprecio.AutoSize = true;
+            this.lblprecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblprecio.Location = new System.Drawing.Point(49, 173);
+            this.lblprecio.Name = "lblprecio";
+            this.lblprecio.Size = new System.Drawing.Size(57, 32);
+            this.lblprecio.TabIndex = 4;
+            this.lblprecio.Text = "Precio:\r\n\r\n";
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(125, 172);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(74, 20);
+            this.txtprecio.TabIndex = 5;
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCalcular.Location = new System.Drawing.Point(114, 333);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(76, 30);
+            this.BtnCalcular.TabIndex = 6;
+            this.BtnCalcular.Text = "calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = true;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonto.Location = new System.Drawing.Point(52, 226);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(54, 32);
+            this.lblMonto.TabIndex = 7;
+            this.lblMonto.Text = "Monto:\r\n\r\n";
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(125, 226);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(75, 20);
+            this.txtMonto.TabIndex = 8;
+            // 
+            // Compras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(351, 419);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.lblMonto);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.lblprecio);
+            this.Controls.Add(this.txtcantidad);
+            this.Controls.Add(this.lblcantidad);
+            this.Controls.Add(this.lblcalcular);
+            this.Controls.Add(this.lblTitulo1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Compras";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Compras_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo1;
+        private System.Windows.Forms.Label lblcalcular;
+        private System.Windows.Forms.Label lblcantidad;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.Label lblprecio;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.TextBox txtMonto;
+    }
+}
+
---
+++ Compra de 100 unidades/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compra_de_100_unidades
+{
+    public partial class Compras : Form
+    {
+        const double PORC40 = 0.4;//40% de descuento
+        const double PORC20 = 0.2;//20% de descuento
+        const double PORC10 = 0.1;//10% de descuento
+        public Compras()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+
+
+            int und =  Convert.ToInt32(txtcantidad.Text); //Obtener el valor de la caja de texto
+
+
+            double descuento;
+            double monto = 0.0;
+            double precio = Convert.ToDouble(txtprecio.Text); ;
+
+            if (und > 100)
+            {
+                monto = und * precio;
+                descuento = monto * PORC40;
+                monto = monto - descuento;
+            }
+            else
+                if (und >= 25 && und <= 100)
+            {
+                monto = und * precio;
+                descuento = monto * PORC20;
+                monto = monto - descuento;
+            }
+             else 
+                if (und >= 10 &&  und < 25)
+                {
+
+                monto = und * precio;
+                descuento = monto * PORC10;
+                monto = monto - descuento;
+                    }
+
+            txtMonto.Text = monto.ToString();
+
+        }//Fin del mètodo
+
+        private void Compras_Load(object sender, EventArgs e)
+        {
+
+        }
+    } //Fin de declar de clase
+} //Fin del espacio de nombre
